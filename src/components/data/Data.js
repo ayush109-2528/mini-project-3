00b6b53652d67df403350d39446b5703c223aea0
ayush <ayushrajput109@gmail.com>
2023-05-10 22:13:53 +0530

    mini project 2023

---
+++ src/components/data/Data.js
@@ -0,0 +1,297 @@
+export const nav = [
+  {
+    text: "home",
+    path: "/",
+  },
+  {
+    text: "about",
+    path: "/about",
+  },
+  {
+    text: "services",
+    path: "/services",
+  },
+  {
+    text: "blog",
+    path: "/blog",
+  },
+  {
+    text: "pricing",
+    path: "/pricing",
+  },
+  {
+    text: "contact",
+    path: "/contact",
+  },
+]
+export const featured = [
+  {
+    cover: "../images/hero/h1.png",
+    name: "Family House",
+    total: "122 Property",
+  },
+  {
+    cover: "../images/hero/h2.png",
+    name: "House & Villa",
+    total: "155 Property",
+  },
+  {
+    cover: "../images/hero/h3.png",
+    name: "Apartment",
+    total: "300 Property",
+  },
+  {
+    cover: "../images/hero/h4.png",
+    name: "Office & Studio",
+    total: "80 Property",
+  },
+  {
+    cover: "../images/hero/h6.png",
+    name: "Villa & Condo",
+    total: "80 Property",
+  },
+]
+export const list = [
+  {
+    id: 1,
+    cover: "../images/list/p-1.png",
+    name: "Red Carpet Real Estate",
+    location: "210 Zirak Road, mathura",
+    category: "For Rent",
+    price: "Rs 3,700",
+    type: "Apartment",
+  },
+  {
+    id: 2,
+    cover: "../images/list/p-2.png",
+    name: "Fairmount Properties",
+    location: "5698 Zirak Road, delhi",
+    category: "For Sale",
+    price: "Rs 9,750",
+    type: "Condos",
+  },
+  {
+    id: 3,
+    cover: "../images/list/p-7.png",
+    name: "The Real Estate Corner",
+    location: "5624 Mooker Market, Mainpuri",
+    category: "For Rent",
+    price: "Rs 5,860",
+    type: "Offices",
+  },
+  {
+    id: 4,
+    cover: "../images/list/p-4.png",
+    name: "Herringbone Realty",
+    location: "5621 Liverpool, agra",
+    category: "For Sale",
+    price: "Rs 7,540",
+    type: "Homes & Villas",
+  },
+  {
+    id: 5,
+    cover: "../images/list/p-5.png",
+    name: "Brick Lane Realty",
+    location: "210 Montreal Road, mathura",
+    category: "For Rent",
+    price: "Rs 4,850",
+    type: "Commercial",
+  },
+  {
+    id: 6,
+    cover: "../images/list/p-6.png",
+    name: "Banyon Tree Realty",
+    location: "210 Zirak Road, mathura",
+    category: "For Sale",
+    price: "Rs 2,742",
+    type: "Apartment",
+  },
+]
+export const awards = [
+  {
+    icon: <i class='fa-solid fa-trophy'></i>,
+    num: "32 M	",
+    name: "Blue Burmin Award",
+  },
+  {
+    icon: <i class='fa-solid fa-briefcase'></i>,
+    num: "43 M",
+    name: "Mimo X11 Award",
+  },
+  {
+    icon: <i class='fa-solid fa-lightbulb'></i>,
+    num: "51 M",
+    name: "Australian UGC Award",
+  },
+  {
+    icon: <i class='fa-solid fa-heart'></i>,
+    num: "42 M",
+    name: "IITCA Green Award",
+  },
+]
+export const location = [
+  {
+    id: 1,
+    name: "lucknow",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-1.png",
+  },
+  {
+    id: 2,
+    name: "lucknow",
+    Villas: "12 Villas",
+    Apartments: "10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-2.png",
+  },
+  {
+    id: 3,
+    name: "agra",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-3.png",
+  },
+  {
+    id: 4,
+    name: "delhi",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-4.png",
+  },
+  {
+    id: 5,
+    name: "mathura",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-5.png",
+  },
+  {
+    id: 6,
+    name: "Mainpuri",
+    Villas: "12 Villas",
+    Apartments: " 10 Apartments",
+    Offices: "07 Offices",
+    cover: "./images/location/city-6.png",
+  },
+]
+export const team = [
+  {
+    list: "80",
+    cover: "../images/customer/team-3.jpg",
+    address: "Raebareli",
+    name: "Abhinav",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "42",
+    cover: "../images/customer/team-5.jpg",
+    address: "Mainpuri",
+    name: "Ayush",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+  {
+    list: "38",
+    cover: "../images/customer/team-5.jpg",
+    address: "Raebareli",
+    name: "Nagendra",
+    icon: [<i class='fa-brands fa-facebook-f'></i>, <i class='fa-brands fa-linkedin'></i>, <i class='fa-brands fa-twitter'></i>, <i class='fa-brands fa-instagram'></i>],
+  },
+]
+export const price = [
+  {
+    plan: "Basic",
+    price: "2900",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "120GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "5GB Cloud Storage",
+      },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "personal Help Support" },
+      { change: "color", icon: <i class='fa-solid fa-x'></i>, text: "Enterprise SLA" },
+    ],
+  },
+  {
+    best: "Best Value",
+    plan: "Standard",
+    price: "4900",
+    ptext: "per user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "99.5% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "150GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "10GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "personal Help Support",
+      },
+      {
+        change: "color",
+        icon: <i class='fa-solid fa-x'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+  {
+    plan: "Platinum",
+    price: "7900",
+    ptext: "2 user, per month",
+    list: [
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "100% Uptime Guarantee",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "200GB CDN Bandwidth",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "20GB Cloud Storage",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Personal Help Support",
+      },
+      {
+        icon: <i class='fa-solid fa-check'></i>,
+        text: "Enterprise SLA",
+      },
+    ],
+  },
+]
+export const footer = [
+  {
+    title: "LAYOUTS",
+    text: [{ list: "Home Page" }, { list: "About Page" }, { list: "Service Page" }, { list: "Property Page" }, { list: "Contact Page" }, { list: "Single Blog" }],
+  },
+  {
+    title: "ALL SECTIONS",
+    text: [{ list: "Header " }, { list: "Features" }, { list: "Attractive" }, { list: "Testimonials" }, { list: "Videos" }, { list: "Footers" }],
+  },
+  {
+    title: "COMPANY",
+    text: [{ list: "About" }, { list: "Blog" }, { list: "Pricing" }, { list: "Affiliate" }, { list: "Login" }, { list: "Changelog" }],
+  },
+]
